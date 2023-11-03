c208f9fc77058aef48f3bb6ac424892e6a96839e
Doanh Tran <s3927023@rmit.edu.vn>
2023-11-03 13:48:40 +1100

    route error

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import { Route } from "react-router-dom";
+
+import AllMeetupsPage from "./pages/AllMeetups";
+import FavoritesPage from "./pages/Favorites";
+import NewMeetupPage from "./pages/NewMeetup";
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Route path="/">
+        <AllMeetupsPage />
+      </Route>
+      <Route path="/">
+        <FavoritesPage />
+      </Route>
+      <Route>
+        <NewMeetupPage />
+      </Route>
+    </div>
+  );
 }
 
 export default App;
