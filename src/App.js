31237c67c22799cae36c8e1760a0941916e33c35
Doanh Tran <s3927023@rmit.edu.vn>
2023-11-08 00:45:29 +1100

    Updated MainNavigation.js and MainNavigation.module.css

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import AllMeetupsPage from "./pages/AllMeetups";
 import FavoritesPage from "./pages/Favorites";
 import NewMeetupPage from "./pages/NewMeetup";
+import MainNavigation from "./components/Layout/MainNavigation";
 
 function App() {
   return (
       // fixed with this reference: https://bobbyhadz.com/blog/react-useroutes-may-be-used-only-in-context-of-router
     <Router>
       <div>
+        <MainNavigation/>
         <Routes>
           {/*reference to this: https://stackoverflow.com/questions/63124161/attempted-import-error-switch-is-not-exported-from-react-router-dom */}
           <Route path="/" exact element={<AllMeetupsPage />}/>
