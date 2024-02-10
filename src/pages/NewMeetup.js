fa5c1f32a4f27c77934ffacad3d0cf88967226a3
Doanh Tran <s3927023@rmit.edu.vn>
2024-02-10 20:48:58 +0700

    adding a backend: prepare app for http request and sending POST htpp request

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,9 +1,21 @@
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 function NewMeetupPage() {
+  function addMeetupHandler(meetupData) {
+    fetch(
+      "https://reactjs-meetup-app-a6caa-default-rtdb.asia-southeast1.firebasedatabase.app/meetups.json",
+      {
+        method: "POST",
+        body: JSON.stringify(meetupData),
+        headers: {
+          "Content-type": "application/json",
+        },
+      }
+    );
+  }
   return (
     <section>
       <h1>Add New Meetup</h1>
-      <NewMeetupForm />
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
     </section>
   );
 }
