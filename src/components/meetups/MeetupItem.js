842f52b79dcf127102e7abff6af24d793c17c74d
Doanh Tran <s3927023@rmit.edu.vn>
2024-03-09 22:34:13 +0700

    useConext practice

--- src/components/meetups/MeetupItem.js
+++ src/components/meetups/MeetupItem.js
@@ -1,24 +1,30 @@
+import { useContext } from "react";
 import classes from "./MeetupItem.module.css";
 import Card from "../ui/Card";
+import FavoritesContext from "../../store/favorites-context";
 
 function MeetupItem(props) {
-    return (
-        <li className={classes.item}>
-            <Card>
-                <div className={classes.image}>
-                    <img src={props.image} alt={props.title}/>
-                </div>
-                <div className={classes.content}>
-                    <h3>{props.title}</h3>
-                    <address>{props.address}</address>
-                    <p>{props.description}</p>
-                </div>
-                <div className={classes.actions}>
-                    <button>To Favorites</button>
-                </div>
-            </Card>
-        </li>
-    );
+  const favoriteCtx = useContext(FavoritesContext);
+  const itemIsFavorite = favoriteCtx.itemIsFavorite(props.id);
+  function toggleFavoriteStatusHandler() {}
+
+  return (
+    <li className={classes.item}>
+      <Card>
+        <div className={classes.image}>
+          <img src={props.image} alt={props.title} />
+        </div>
+        <div className={classes.content}>
+          <h3>{props.title}</h3>
+          <address>{props.address}</address>
+          <p>{props.description}</p>
+        </div>
+        <div className={classes.actions}>
+          <button onClick={toggleFavoriteStatusHandler}>To Favorites</button>
+        </div>
+      </Card>
+    </li>
+  );
 }
 
 export default MeetupItem;
---
+++ src/store/favorites-context.js
@@ -0,0 +1,45 @@
+import { createContext, useState } from "react";
+
+const FavoritesContext = createContext({
+  favorites: [],
+  totalFavorites: 0,
+  addFavorites: (favoriteMeetup) => {},
+  removeFavorties: (meetupId) => {},
+  itemIsFavorite: (meetupId) => {},
+});
+
+export function FavoritesContextProvider(props) {
+  const [userFavorites, setUserFavorites] = useState([]);
+
+  function addFavoritesHandler(favoriteMeetup) {
+    setUserFavorites((prevUserFavorites) => {
+      return prevUserFavorites.concat(favoriteMeetup);
+    });
+  }
+
+  function removeFavoritesHandler(meetupId) {
+    setUserFavorites((prevUserFavorites) => {
+      return prevUserFavorites.filter((meetup) => meetup.id !== meetupId);
+    });
+  }
+
+  function itemIsFavoritesHandler(meetupId) {
+    return userFavorites.some((meetup) => meetup.id === meetupId);
+  }
+
+  const context = {
+    favorites: userFavorites,
+    totalFavorites: userFavorites.length,
+    addFavorites: addFavoritesHandler,
+    removeFavorties: removeFavoritesHandler,
+    itemIsFavorite: itemIsFavoritesHandler,
+  };
+
+  return (
+    <FavoritesContext.Provider value={context}>
+      {props.children}
+    </FavoritesContext.Provider>
+  );
+}
+
+export default FavoritesContext;
