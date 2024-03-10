14a58e860f87a4ea184d4bea30cbee6856db0f09
Doanh Tran <s3927023@rmit.edu.vn>
2024-03-10 13:55:07 +0700

    more context usage

--- src/components/Layout/MainNavigation.js
+++ src/components/Layout/MainNavigation.js
@@ -1,26 +1,35 @@
 import { Link } from "react-router-dom";
+import { useContext } from "react";
+
 import MainNav from "./MainNavigation.module.css";
+import FavoritesContext from "../../store/favorites-context";
 
 function MainNavigation() {
-    return (
-        <header className={MainNav.header}>
-            <div className={MainNav.logo}>
-                React Meetups
-            </div>
-            <nav>
-                <ul>
-                    <li>
-                        <Link to={"/"}> All Meetups</Link>
-                    </li>
-                    <li>
-                        <Link to={"/favourites"}> Favorite Meetups</Link>
-                    </li>
-                    <li>
-                        <Link to={"/new-meetup"}> Add New Meetups</Link>
-                    </li>
-                </ul>
-            </nav>
-        </header>)
+  const favoriteCtx = useContext(FavoritesContext);
+  return (
+    <header className={MainNav.header}>
+      <div className={MainNav.logo}>React Meetups</div>
+      <nav>
+        <ul>
+          <li>
+            <Link to={"/"}> All Meetups</Link>
+          </li>
+          <li>
+            <Link to={"/favourites"}>
+              Favorite Meetups
+              <span className={MainNavigation.module.css.badge}>
+                {FavoritesContext.totalFavorites}
+              </span>
+              {/* check again */}
+            </Link>
+          </li>
+          <li>
+            <Link to={"/new-meetup"}> Add New Meetups</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
 
 export default MainNavigation;
--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,7 +1,23 @@
+import { useContext } from "react";
+import FavoritesContext from "../store/favorites-context";
+import MeetupList from "../components/meetups/MeetupList";
+
 function FavoritesPage() {
-    return (
-        <div>Favorite Page</div>
-    );
+  const favoritesCtx = useContext(FavoritesContext);
+  let content;
+
+  if (favoritesCtx.totalFavorites === 0) {
+    content = <p>No Fav yet! Wanna add some?</p>;
+  } else {
+    content = <MeetupList meetups={favoritesCtx.favorites} />;
+  }
+
+  return (
+    <section>
+      <h1>My Favorites</h1>
+      {content}
+    </section>
+  );
 }
 
 export default FavoritesPage;
